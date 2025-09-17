cde2c8ab3a5cb49a8792af7b461fcc59c124044b
Babajide Oluwaferanmi <babajideoluwaferanmi301@gmail.com>
2025-09-17 23:34:57 +0100

    adding my latest projects

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,13 @@ import Footer from "./components/Footer";
 
 function App() {
   const skills = [
-    "HTML5",
-    "CSS3",
+    "React",
+    "Redux",
+    "Next.js",
+    "Express.js",
+    "Prisma",
     "JavaScript",
     "TypeScript",
-    "React",
     "Tailwind CSS",
     "Git",
     "Firebase",
@@ -18,12 +20,28 @@ function App() {
 
   const projects = [
     {
+      title: "Chatty",
+      description:
+        "A real-time chat application built with Next.js for the frontend, Express.js for the backend using Socket.io for real-time communication.",
+      tech: ["Next.js", "Express.js", "Socket.io", "Prisma"],
+      github: "https://github.com/babaj301/chat-backend",
+      live: "https://chat-frontend-ten-opal.vercel.app/rooms",
+    },
+    {
       title: "TaskMinder",
       description:
         "A full-featured task management application built with TypeScript and Redux. Features include CRUD operations, Firebase Authentication for Google Sign Up and Login, and Firestore integration.",
       tech: ["TypeScript", "Redux", "Firebase", "Tailwind CSS"],
       github: "https://github.com/babaj301/task-dashboard",
       live: "https://taskrrrr.netlify.app/",
+    },
+    {
+      title: "CreoWis",
+      description:
+        "A responsive landing page for a note taking app using Next.js, and Framer Motion for animations. Built with TypeScript and Tailwind CSS for styling.",
+      tech: ["Next.js", "TypeScript", "Tailwind CSS", "Framer Motion"],
+      github: "https://github.com/babaj301/creowis-landing",
+      live: "https://creowis-landing.vercel.app/",
     },
     {
       title: "E-commerce Site",
